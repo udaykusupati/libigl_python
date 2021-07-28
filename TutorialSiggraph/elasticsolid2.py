8f7c3ec94868e289a6cd1b61d3c0f5c4b42b3744
Quentin Becker <quentin.becker@gmail.com>
2021-07-28 15:00:03 +0200

    Remove conjugate gradient from elastic solid

--- TutorialSiggraph/elasticsolid2.py
+++ TutorialSiggraph/elasticsolid2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import sparse
 import igl
+from Utils import conjugate_gradient
 
 
 # -----------------------------------------------------------------------------
@@ -220,56 +221,10 @@ class ElasticSolid(object):
                         (1 + self.gamma / dt) * self.compute_force_differentials(dv))
 
             RHS = 1 / dt * np.einsum('ij,jk->ik', M, self.velocity - new_velocity) + ft
-            dv = self.conjugate_gradient(LHS, RHS)
+            dv = conjugate_gradient(LHS, RHS)
             self.displace(dv)
             new_velocity += dv / dt
         self.velocity = new_velocity
-
-    def conjugate_gradient(self, Ax_method, b, x0=None, eps=1e-3, max_steps=None):
-        '''
-        Solves Ax = b where A is positive definite.
-        A has shape (dim, dim), x and b have shape (dim, n).
-
-        Input:
-        - Ax_method : a method that takes x and returns the product Ax
-        - b         : right hand side of the linear system
-        - x0        : initial guess for x0 (defaults to 0s)
-        - eps       : tolerance on the residual magnitude
-        - max_steps : maximum number of iterations (defaults to dim)
-
-        Output:
-        - xStar : np array of shape (dim, n) solving the linear system
-        '''
-
-        dim, n = b.shape
-        if max_steps is None:
-            max_steps = dim
-        if x0 is None:
-            xStar = np.zeros_like(b)
-        else:
-            xStar = x0
-        residuals = b - Ax_method(xStar)
-        directions = residuals
-        # Reusable quantities
-        A_directions = Ax_method(directions)  # A p
-        resNormsSq = np.sum(residuals ** 2, axis=0)  # r^T r
-        dirNormsSqA = np.zeros(shape=(n,))  # stores p^T A p
-        for i in range(n):
-            dirNormsSqA[i] = directions[:, i] @ A_directions[:, i]
-        for k in range(max_steps):
-            alphas = resNormsSq / dirNormsSqA
-            xStar = xStar + directions * alphas
-            residuals = residuals - A_directions * alphas
-            newResNormsSq = np.sum(residuals ** 2, axis=0)
-            if max(np.sqrt(newResNormsSq)) < eps:
-                break
-            betas = newResNormsSq / resNormsSq
-            directions = residuals + directions * betas
-            A_directions = Ax_method(directions)
-            resNormsSq = newResNormsSq
-            for i in range(n):
-                dirNormsSqA[i] = directions[:, i] @ A_directions[:, i]
-        return xStar
 
 # -----------------------------------------------------------------------------
 #                                    Test
