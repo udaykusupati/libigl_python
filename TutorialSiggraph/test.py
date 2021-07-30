813f380746c86e309bda0da95e0f0f19b26ea43f
Quentin Becker <quentin.becker@gmail.com>
2021-07-30 18:19:08 +0200

    Provide the skeleton of the testing file

---
+++ TutorialSiggraph/test.py
@@ -0,0 +1,110 @@
+from elasticsolid2 import *
+from elasticenergy import *
+from mayavi import mlab
+import igl
+
+def test_jacobian_translation(solid):
+    dv_def = np.zeros_like(solid.v)
+    dv_def[:, 0] += 1
+
+    # Updates the Jacobian
+    solid.update_shape(solid.v + dv_def)
+
+    # Should be the identity
+    target_F = np.tile(np.eye(3).reshape(1, 3, 3), (solid.t.shape[0], 1, 1))
+
+    assert np.allclose(target_F, solid.F)
+    pass
+
+def test_jacobian(solid):
+    F = np.array([
+        [1, 0, 1],
+        [0, 2, 0],
+        [0, 0, 3]
+    ])
+
+    v_def = solid.v @ F.T
+    # Updates the Jacobian
+    solid.update_shape(v_def)
+
+    target_F = np.tile(F.reshape(1, 3, 3), (solid.t.shape[0], 1, 1))
+    assert np.allclose(target_F, solid.F)
+
+    pass
+
+def test_mass_matrix():
+    pass
+
+def test_strain_tensor(solid):
+    F = np.array([
+        [1, 0, 1],
+        [0, 2, 0],
+        [0, 0, 3]
+    ])
+
+    v_def = solid.v @ F.T
+    # Updates the Jacobian
+    solid.update_shape(v_def)
+
+    if type(solid.ee) == LinearElasticEnergy:
+        E = np.array([
+            [0  , 0, 1/2],
+            [0  , 1, 0  ],
+            [1/2, 0, 2  ]
+        ])
+    
+    target_E = np.tile(E.reshape(1, 3, 3), (solid.t.shape[0], 1, 1))
+    assert np.allclose(target_E, solid.ee.E)
+
+    pass
+
+def test_stress_tensor():
+    pass
+
+def test_von_mises():
+    pass
+
+if __name__ == '__main__':
+
+    # Some characteristics
+    rho     = 1  # [kg.m-3]
+    damping = 0.
+
+    # Choose E and nu, so that mu = lambda = 1
+    young   = 2.5 
+    poisson = 0.25
+
+    t = np.array([
+        [0, 1, 2, 3],
+        [1, 2, 3, 4]
+    ])
+    v = np.array([
+        [0., 0., 0.],
+        [1., 0., 0.],
+        [0., 1., 0.],
+        [0., 0., 1.],
+        [2/3, 2/3, 2/3]
+    ])
+
+    tb = igl.boundary_facets(t)
+    M  = mlab.triangular_mesh(v[:, 0], v[:, 1], v[:, 2], tb)
+    # mlab.show()
+
+    ee    = LinearElasticEnergy(young, poisson)
+
+    # Test independence to translation
+    solid = ElasticSolid(v, t, ee, rho=rho, damping=damping, 
+                         pin_idx=[], f_ext=None, self_weight=True)
+    test_jacobian_translation(solid)
+
+    # Test Jacobian computation
+    solid = ElasticSolid(v, t, ee, rho=rho, damping=damping, 
+                         pin_idx=[], f_ext=None, self_weight=True)
+    test_jacobian(solid)
+
+    # Test strain tensor computation
+    solid = ElasticSolid(v, t, ee, rho=rho, damping=damping, 
+                         pin_idx=[], f_ext=None, self_weight=True)
+    test_strain_tensor(solid)
+
+    pass
